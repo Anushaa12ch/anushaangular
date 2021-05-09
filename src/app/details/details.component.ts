654b9452830a791cb91c221ff2c66d6c48144f0d
Anushaa <anushaanetha@gmail.com>
2021-05-09 12:24:27 +0530

    first commit

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  //creating an array
+  products=[1,2,3,4,5,6];
+}
